a1402edbb88542ec6d623294c593afac9a4c292a
bobo-1232 <javierr8906@gmail.com>
2023-02-13 15:35:27 -0600

    Rough Draft of skills page

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from gitlab import get_commits, get_issues, get_issues_by_user
 
 #Dummy Data for Phase 1
 job = [{'Title': 'Senior Software Engineer', 'Company': 'Google', 'Location': 'San Francisco, CA', 'Link': 'https://careers.google.com/jobs/results/116958901158453958-senior-software-engineering-manager-ar/?f=true&page=29&utm_campaign=google_jobs_apply&utm_medium=organic&utm_source=google_jobs_apply', 'Date':'2/13/2023'},\
- {'Title': 'Data Analyst', 'Company': 'Simplex', 'Location': 'Austin, TX', 'Link': 'https://jobs.myjobhelper.com/signup?prodid=9874&jobkey=558369-1031810&feedId=3113&pubId=9874&pubFeedId=426&jobType=&pubClickId=374533662&feedOverCap=N&company=Simplex&utm_medium=cpc&utm_source=9874&utm_campaign=xml-426&utm_content=Data%20Analyst&utm_term=Simplex&l=&ccuid=', 'Date':'2/12/2023'},\
- {'Title': 'SR Software Developer', 'Company': 'Spectrum', 'Location': 'Austin, TX', 'Link': 'https://spectrum.talentify.io/job/sr-software-developer-austin-texas-spectrum-charter-communications-332670br?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic', 'Date':'2/13/2023'}]
+    {'Title': 'Data Analyst', 'Company': 'Simplex', 'Location': 'Austin, TX', 'Link': 'https://jobs.myjobhelper.com/signup?prodid=9874&jobkey=558369-1031810&feedId=3113&pubId=9874&pubFeedId=426&jobType=&pubClickId=374533662&feedOverCap=N&company=Simplex&utm_medium=cpc&utm_source=9874&utm_campaign=xml-426&utm_content=Data%20Analyst&utm_term=Simplex&l=&ccuid=', 'Date':'2/12/2023'},\
+    {'Title': 'SR Software Developer', 'Company': 'Spectrum', 'Location': 'Austin, TX', 'Link': 'https://spectrum.talentify.io/job/sr-software-developer-austin-texas-spectrum-charter-communications-332670br?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic', 'Date':'2/13/2023'}]
 
-skill = [{'Name': '', 'Certifications':'', 'Skill':'', 'Industry':''}]
+skill = [{'Name': 'Excel', 'Certifications':'Microsoft Office Specialist', 'Skill':'Hard', 'Industry':'Any'},\
+    {'Name': 'Public Speaking', 'Certifications':'Expierence', 'Skill':'Soft', 'Industry':'Any'},\
+    {'Name': '3D AutoCad', 'Certifications':'Inventor', 'Skill':'Hard', 'Industry':'Engineering'}]
+
 location = [{'City':'', 'State':'', 'Unemployment':'', 'Salary':'', 'Rent':''}]
 course = [{'Name':'', 'Time':'', 'Cost':'', 'Provider':'', 'Link':''}]
 
