1b2220157ade1764a76dc50cdc872133f1e0a9f4
wresendiz <wresendiz@utexas.edu>
2023-02-16 20:49:18 -0600

    corrected  data models and pages

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from gitlab import get_commits, get_issues
+import data_dict
 
-# Dummy Data for Phase 1
-job = [{'Title': 'Senior Software Engineer', 'Company': 'Google', 'Location': 'San Francisco, CA', 'Link': 'https://careers.google.com/jobs/results/116958901158453958-senior-software-engineering-manager-ar/?f=true&page=29&utm_campaign=google_jobs_apply&utm_medium=organic&utm_source=google_jobs_apply', 'Date':'2/13/2023'},\
-    {'Title': 'Data Analyst', 'Company': 'Simplex', 'Location': 'Austin, TX', 'Link': 'https://jobs.myjobhelper.com/signup?prodid=9874&jobkey=558369-1031810&feedId=3113&pubId=9874&pubFeedId=426&jobType=&pubClickId=374533662&feedOverCap=N&company=Simplex&utm_medium=cpc&utm_source=9874&utm_campaign=xml-426&utm_content=Data%20Analyst&utm_term=Simplex&l=&ccuid=', 'Date':'2/12/2023'},\
-    {'Title': 'SR Software Developer', 'Company': 'Spectrum', 'Location': 'Austin, TX', 'Link': 'https://spectrum.talentify.io/job/sr-software-developer-austin-texas-spectrum-charter-communications-332670br?utm_campaign=google_jobs_apply&utm_source=google_jobs_apply&utm_medium=organic', 'Date':'2/13/2023'}]
-
-skill = [{'Name': 'Excel', 'Certifications':'Microsoft Office Specialist', 'Skill':'Hard', 'Industry':'Any'},\
-    {'Name': 'Public Speaking', 'Certifications':'Expierence', 'Skill':'Soft', 'Industry':'Any'},\
-    {'Name': '3D AutoCad', 'Certifications':'Inventor', 'Skill':'Hard', 'Industry':'Engineering'}]
-
-locations = [{'City':'Austin', 'State':'TX', 'Unemployment':'6', 'Salary':'$52,500', 'Rent':'$1,100'},\
-    {'City':'San Francisco', 'State':'CA', 'Unemployment':'8', 'Salary':'$63,250', 'Rent':'$1,600'},\
-    {'City':'Chicago', 'State':'IL', 'Unemployment':'7.5', 'Salary':'$61,500', 'Rent':'$1,550'}]
-
-course = [{'Name':'CS50: Introduction to Computer Science', 'Time':'10-20 hours per week', 'Cost':'$149.00', 'Provider':'Harvard', 'Link':'https://pll.harvard.edu/course/cs50-introduction-computer-science?delta=0'},\
-    {'Name':'The Complete Digital Marketing Course', 'Time':'6 hours per week', 'Cost':'$149.99', 'Provider':'Udemy', 'Link':'https://www.udemy.com/course/learn-digital-marketing-course/'},\
-    {'Name':'Google Project Management: Professional Certificate', 'Time':'10 hours per week', 'Cost':'$300.00', 'Provider':'Google', 'Link':'https://www.coursera.org/professional-certificates/google-project-management?utm_source=google&utm_medium=institutions&utm_campaign=gwgsite-gDigital-paidha-sem-sk-gen-exa-glp-null&_ga=2.75832981.1032362299.1676326416-957183620.1676326416&_gac=1.122856953.1676326423.CjwKCAiA3KefBhByEiwAi2LDHMn3d5dlkyBfBWUoU4Uco7Tx4O2yb-hR1ZNVxSkvggxK4tupjyDC3hoCFpIQAvD_BwE'}]
-
-# These are the headers of the tables
-job_headers = ['Job Title', 'Company', 'Location', 'Link to Application', 'Job Posting Date']
-skill_headers = ['Skill Name', 'Certifications', 'Skill Type', 'Industries']
-location_headers = ['City', 'State', 'Unemployment Rate', 'Average Salary', 'Average Rent']
-course_headers = ['Course Name', 'Time to Complete', 'Cost', 'Provider', 'Link to Course']
 
 def create_app():
 	
@@ -40,19 +19,40 @@ def create_app():
 
 	@app.route('/jobs')
 	def jobs():
-		return render_template('jobs.html', job = job, headers = job_headers)
+		return render_template('jobs.html', jobs = data_dict.jobs)
+	
+	@app.route('/jobs/<job_id>')
+	def view_job(job_id):
+		job = data_dict.jobs[job_id]
+		return render_template('job_details.html', job = job)
 
 	@app.route('/skills')
 	def skills():
-		return render_template('skills.html', skill = skill, headers = skill_headers)
+		return render_template('skills.html', skills = data_dict.skills, courses = data_dict.courses)
+	
+	@app.route('/skills/<skill_id>')
+	def view_skill(skill_id):
+		skill = data_dict.skills[skill_id]
+		return render_template('skill_details.html', skill = skill, courses = data_dict.courses)
 
 	@app.route('/courses')
 	def courses():
-		return render_template('courses.html', course = course, headers =course_headers)
+		return render_template('courses.html', courses = data_dict.courses)
+	
+	@app.route('/courses/<course_id>')
+	def view_course(course_id):
+		course = data_dict.courses[course_id]
+		return render_template('course_details.html', course = course)
 
 	@app.route('/locations')
-	def location():
-		return render_template('locations.html',  locations = locations, headers = location_headers)
+	def locations():
+		return render_template('locations.html',  locations = data_dict.locations)
+	
+	@app.route('/locations/<location_id>')
+	def view_location(location_id):
+		location = data_dict.locations[location_id]
+		# image = images.generate_image(location['name'])
+		return render_template('location_details.html', location = location)
 
 	@app.route('/contact', methods=['GET', 'POST'])
 	def contact():
