5cd32575428b3e555d6b63a16bcaa55c6630c7be
bobo-1232 <javierr8906@gmail.com>
2023-03-14 14:39:40 -0500

    First jobs data file

--- backend/app/data/scrapping_jobs.py
+++ backend/app/data/scrapping_jobs.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import os
 
-states_id = ["new-york", "los-angeles","chicago","houston","phoenix","philadelphia","san-antonio","san-diego","dallas","san-jose","austin","jacksonville","fort-worth","columbus","indianapolis","charlotte","san-francisco","seattle","denver","oklahoma-city","nashville","el-paso","washington","boston","las-vegas","portland","detroit","louisville","memphis","baltimore"]
+cities = ["baltimore"]
+#"new-york", "los-angeles","chicago","houston","phoenix","philadelphia","san-antonio","san-diego","dallas","san-jose","austin","jacksonville","fort-worth","columbus","indianapolis","charlotte","san-francisco","seattle","denver","oklahoma-city","nashville","el-paso","washington","boston","las-vegas","portland","detroit","louisville","memphis",
 clustersCodes = ["4.0000", "6.0000","8.0000", "11.0000", "15.0000"]
 jobsPerClust = 6
 jobsCodes = []    
@@ -22,4 +24,29 @@ for code in clustersCodes:
         code = data[i]['code']
         jobsCodes.append(code)
 
-print(jobsCodes)    
+print(jobsCodes)
+
+for city in cities:
+    blank = 0
+    script_dir = os.path.dirname(__file__)
+    file_path = os.path.join(script_dir, 'jobsByCity/'+city+'Jobs.json')
+
+    with open(file_path,'w') as f:
+        pass            
+    allCurJobs = []
+    
+    for code in jobsCodes:
+        url = "https://api.careeronestop.org/v1/jobsearch/1cgBjWAkajxAu5r/" +code+ "/" +city+ "/25/0/0/0/25/0"
+        payload={}
+        headers = {
+        'Authorization': 'Bearer h5913JTCBlAINrrwwtf+LulXF+a0DQMZ8coPXmT+pF1vlhCajU4FbMvl33gIJ0vbFRIO4XZ1EnUcjJTKp3IWIg=='
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        currentCity = response.json()['Jobs']
+        allCurJobs.append(currentCity)
+
+
+final = json.dumps(allCurJobs, indent=2)
+
+with open(file_path, 'w') as outfile:
+    outfile.write(final)
