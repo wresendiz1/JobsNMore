4beb0e3ce7a9afbb1dffa009bd3aa339eecade56
bobo-1232 <javierr8906@gmail.com>
2023-03-14 15:00:14 -0500

    Added all data files

--- backend/app/data/scrapping_jobs.py
+++ backend/app/data/scrapping_jobs.py
@@ -2,8 +2,7 @@ import requests
 import json
 import os
 
-cities = ["baltimore"]
-#"new-york", "los-angeles","chicago","houston","phoenix","philadelphia","san-antonio","san-diego","dallas","san-jose","austin","jacksonville","fort-worth","columbus","indianapolis","charlotte","san-francisco","seattle","denver","oklahoma-city","nashville","el-paso","washington","boston","las-vegas","portland","detroit","louisville","memphis",
+cities = ["New York, NY", "los-angeles","chicago","houston","phoenix","philadelphia","san-antonio","san-diego","dallas","san-jose","austin","jacksonville","fort-worth","columbus","indianapolis","charlotte","san-francisco","seattle","denver","oklahoma-city","nashville","el-paso","washington","boston","las-vegas","portland","detroit","louisville","memphis", "baltimore"]
 clustersCodes = ["4.0000", "6.0000","8.0000", "11.0000", "15.0000"]
 jobsPerClust = 6
 jobsCodes = []    
@@ -24,8 +23,6 @@ for code in clustersCodes:
         code = data[i]['code']
         jobsCodes.append(code)
 
-print(jobsCodes)
-
 for city in cities:
     blank = 0
     script_dir = os.path.dirname(__file__)
@@ -45,8 +42,8 @@ for city in cities:
         currentCity = response.json()['Jobs']
         allCurJobs.append(currentCity)
 
+  
+    final = json.dumps(allCurJobs, indent=2)
 
-final = json.dumps(allCurJobs, indent=2)
-
-with open(file_path, 'w') as outfile:
-    outfile.write(final)
+    with open(file_path, 'w') as outfile:
+        outfile.write(final)
