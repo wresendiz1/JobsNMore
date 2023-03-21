a466ea71a1abd1798424ed9d429104c06ec6c30a
wresendiz <wresendiz@utexas.edu>
2023-03-20 21:26:35 -0500

    fixed, will remove data_dict

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,8 +8,8 @@ import os
 def create_app():
     app = Flask(__name__)
     
-    from backend.app import data_dict
-    from backend.app import gitlab
+    # from app.data_dict import data_dict
+    from app.gitlab import get_branches, get_commits, get_issues
     
     @app.route("/")
     def index():
@@ -17,54 +17,54 @@ def create_app():
 
     @app.route("/about")
     def about():
-        commits = gitlab.get_commits()
-        issues = gitlab.get_issues()
+        commits = get_commits()
+        issues = get_issues()
         return [commits, issues]
 
     @app.route("/about.json")
     def about_json():
         return send_file("data/about.json")
 
-    @app.route("/jobs")
-    def jobs():
-        # with open("app/jobs.json", "r") as file:
-        #     data = json.load(file)
-        #     jobs = data["Jobs"]
-        #     return jobs
-        job = data_dict.jobs
-        return job
+    # @app.route("/jobs")
+    # def jobs():
+    #     # with open("app/jobs.json", "r") as file:
+    #     #     data = json.load(file)
+    #     #     jobs = data["Jobs"]
+    #     #     return jobs
+    #     job = data_dict.jobs
+    #     return job
 
-    @app.route("/jobs/<job_id>")
-    def view_job(job_id):
-        job = data_dict.jobs[job_id]
-        return job
+    # @app.route("/jobs/<job_id>")
+    # def view_job(job_id):
+    #     job = data_dict.jobs[job_id]
+    #     return job
 
-    @app.route("/skills")
-    def skills():
-        return [data_dict.skills, data_dict.courses]
+    # @app.route("/skills")
+    # def skills():
+    #     return [data_dict.skills, data_dict.courses]
 
-    @app.route("/skills/<skill_id>")
-    def view_skill(skill_id):
-        skill = data_dict.skills[skill_id]
-        return [skill, data_dict.courses]
+    # @app.route("/skills/<skill_id>")
+    # def view_skill(skill_id):
+    #     skill = data_dict.skills[skill_id]
+    #     return [skill, data_dict.courses]
 
-    @app.route("/courses")
-    def courses():
-        return data_dict.courses
+    # @app.route("/courses")
+    # def courses():
+    #     return data_dict.courses
 
-    @app.route("/courses/<course_id>")
-    def view_course(course_id):
-        course = data_dict.courses[course_id]
-        return course
+    # @app.route("/courses/<course_id>")
+    # def view_course(course_id):
+    #     course = data_dict.courses[course_id]
+    #     return course
 
-    @app.route("/locations")
-    def locations():
-        return data_dict.locations
+    # @app.route("/locations")
+    # def locations():
+    #     return data_dict.locations
 
-    @app.route("/locations/<location_id>")
-    def view_location(location_id):
-        location = data_dict.locations[location_id]
-        return location
+    # @app.route("/locations/<location_id>")
+    # def view_location(location_id):
+    #     location = data_dict.locations[location_id]
+    #     return location
 
     @app.route("/contact", methods=["GET", "POST"])
     def contact():
