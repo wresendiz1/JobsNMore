93121790a36f3f470691ffaf796e8b03f2b1f8f5
wresendiz <wresendiz@utexas.edu>
2023-03-21 09:34:48 -0500

    fixed some tables and added inital test cases for DB

--- backend/tests/config.py
+++ backend/tests/config.py
@@ -3,10 +3,12 @@ import pytest
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from app import create_app
+from app import create_app, db
 
 
-@pytest.fixture()
+""" use session to create the app once and then use it for all tests which is faster. Switch to
+function scope if you need to isolate errors between tests."""
+@pytest.fixture(scope="session")
 def app():
     app = create_app()
     app.config.update(
@@ -14,7 +16,12 @@ def app():
             "TESTING": True,
         }
     )
-    yield app
+    with app.app_context():
+        yield app
+        # teardown
+        db.session.remove()
+        db.drop_all()
+        
 
 
 @pytest.fixture()
@@ -30,3 +37,22 @@ def runner(app):
 def test_request_example(client):
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_postgres_query():
+    from app.models import Location
+    city = db.session.execute(db.select(Location).filter_by(CityID = 1)).scalar_one()
+    assert city.City == 'New York'
+        
+def test_my_feature():
+    from app.models import Course
+    
+    s = Course(Id = "123", OnetCode = "123", Provider = "123", Name = "123", Url = "123", Type = "123", Description = "123")
+    db.session.add(s)
+    db.session.commit()
+    r = db.session.query(Course).filter_by(Id = '123').one()
+    
+    assert r.Id == '123'          
+    
+    r = db.session.query(Course).filter_by(Id = '123').delete()
+    db.session.commit()
