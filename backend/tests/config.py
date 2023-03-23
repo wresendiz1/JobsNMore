d4ab2425d5eb71d1e6c4dc647ab5a433ad7598e8
Jesus <jesusamc@utexas.edu>
2023-03-22 19:33:49 -0500

    Added unit tests
    Closes #20

--- backend/tests/config.py
+++ backend/tests/config.py
@@ -5,7 +5,8 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from app import create_app, db 
 from app.models import Location, Job, Occupation, Industry, Course, Tech_Skill, Dtech_Skill, Basic_Skill, Dbasic_Skill
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, DataError
+import random
 # fmt: on
 
 """ use session to create the app once and then use it for all tests which is faster. Switch to
@@ -47,15 +48,7 @@ def test_request_example(client):
     response = client.get("/")
     assert response.status_code == 200
 
-
-def test_postgres_query(database):
-    city = database.session.execute(
-        database.select(Location).filter_by(CityID=1)
-    ).scalar_one()
-    assert city.City == "New York"
-
-
-def test_db_relations(database):
+def test_db_inserting_course_incorrect(database):
     # Raise an error as there is a foreign key constraint from course to occupation
     # start a new session to test the error
     with pytest.raises(IntegrityError):
@@ -75,8 +68,7 @@ def test_db_relations(database):
     """ NOTE: we dont have to delete the course as it will be deleted when the session is closed 
     as long as we dont commit"""
 
-
-def test_db_inserting(database):
+def test_db_inserting_course(database):
     # from app.models import Course
 
     case = Course(
@@ -92,3 +84,39 @@ def test_db_inserting(database):
     database.session.add(case)
     r = database.session.query(Course).filter_by(Id="1123").one()
     assert r.Id == "1123"
+
+def test_db_querying_course(database):
+	course = database.session.query(Course).filter_by(Id = '3998-B').one()
+	assert course.Type == 'Advanced'
+
+def test_db_vartype_course(database):
+	entries = Course.query.all()
+	testEntry = random.choice(entries)
+
+	assert isinstance(testEntry.Id,str)
+	assert isinstance(testEntry.OnetCode,str)
+	assert isinstance(testEntry.Provider,str)
+	assert isinstance(testEntry.Name,str)
+	assert isinstance(testEntry.Url,str)
+	assert isinstance(testEntry.Type,str)
+	assert isinstance(testEntry.Description,str) or isinstance(testEntry.Description,None) 
+
+def test_db_inserting_location(database):
+	#Case with right type of variables
+	case = Location(City='Los Santos', State='San Andreas', Population=1234, Budget='Low', Safety='Low', Average_rat=5, Guide='fakewebsite.com', CityID=42, Photos={'photo.png'})
+
+	database.session.add(case)
+	r = database.session.query(Location).filter_by(City='Los Santos').one()
+	assert r.CityID == 42
+
+def test_db_queryingType_location(database):
+	with pytest.raises(DataError):
+		#Indexing data by the wrong type of variable
+		location = database.session.execute(database.select(Location).filter_by(CityID='one')).one()
+
+def test_postgres_query_location(database):
+    city = database.session.execute(
+        database.select(Location).filter_by(CityID=1)
+    ).scalar_one()
+    assert city.City == "New York"
+
