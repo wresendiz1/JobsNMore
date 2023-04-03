448d4406302939bb6e0ae74fd6505775c932fc89
cadetaylor01 <cade.taylor01@gmail.com>
2023-04-02 21:15:30 -0500

    Fixing unit test

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -146,7 +146,7 @@ def test_adding_course(database):
     assert course.Description == "A test course"
 
 def test_postgres_query_course(database):
-    with pytest.raises(IndexError):
+    with pytest.raises(ValueError):
         course = database.session.execute(
             database.select(Course).filter_by(Name="The Cheese Course")
         ).one()
